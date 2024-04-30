3787e3fe611e47d603a169c71480d8de58c579e1
Orkhan1990 <orkhan.talibli@gmail.com>
2024-04-30 16:32:44 +0400

    Sign up page ui done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import NoPage from "./pages/NoPage";
+import SignUp from "./pages/SignUp";
 function App() {
 
   return (
@@ -10,6 +11,7 @@ function App() {
     
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="*" element={<NoPage />} />
     </Routes>
   </Router>
--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
-const SignIn = () => {
+import { Button, Label, TextInput } from "flowbite-react";
+const SignUp = () => {
   return (
     <div className='min-h-screen image'>
         <div className='bgFon min-h-screen flex justify-center items-center'>
             <div className='text-white w-[350px]'>
-                <h1 className='text-center text-5xl font-semibold'>Sign In</h1>
+                <h1 className='text-center text-5xl font-semibold'>Sign Up</h1>
                 <form  className='flex flex-col gap-3 mt-5'>
                     <div>
                     <Label htmlFor="username" value="Username"  className='text-white text-md'/>
                     <TextInput id="username" type="text" placeholder="Username" required />
                     </div>
                     <div>
+                    <Label htmlFor="email" value="Email"  className='text-white text-md'/>
+                    <TextInput id="email" type="email" placeholder="Email" required />
+                    </div>
+                    <div>
                     <Label htmlFor="password" value="Password" className='text-white text-md'/>
                     <TextInput id="password" type="password"  />
                     </div>
-                    <Button>Sign In</Button>
+                    <Button>Sign Up</Button>
                 </form>
             </div>
         </div>
@@ -23,4 +27,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+export default SignUp
